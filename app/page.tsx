10cce8d97a4c75cbd4080c4821f47a656c180dac
zohaib akhtar <za809475@gmail.com>
2023-11-24 00:11:39 +0500

    created file structure of Travel website

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="font-bold text-teal-500 text-2xl">
+    <h1>hellow world !</h1>
+    </main>
+  )
+}
